d29693718f37b8f28e41398763d0560e7c71462c
U-mdarczoom3\mdarc3 <grantbow@mdarc.org>
2026-02-22 16:40:07 -0800

    20260222 builds, dependencies need work

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import eslint from '@nabla/vite-plugin-eslint';
 
 export default defineConfig({
   plugins: [
