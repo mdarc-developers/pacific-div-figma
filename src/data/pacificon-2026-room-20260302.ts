94973f5cbef95203116f4b583c1fcaf4696f1eee
U-mdarczoom3\mdarc3 <grantbow@mdarc.org>
2026-03-03 11:25:39 -0800

    20260303 pacificon hotel coordinates!

--- src/data/pacificon-2026-room-20260302.ts
+++ src/data/pacificon-2026-room-20260302.ts
@@ -7,152 +7,193 @@ export const mapRooms: [string, Room[]] = [
   //origWidthNum: 983,
   [
     {
-      name: "Registration",
-      coords: [
-        [1, 3],
-        [57, 3],
-        [57, 90],
-        [1, 90],
-      ], // Bottom Left
+      name: "Registration", // new
+      coords: [[557, 322],
+        [609, 322],
+        [609, 387],
+        [557, 387]
+      ],
       color: "#10B981",
     },
     {
-      name: "Prize Booth",
-      coords: [
-        [1, 3],
-        [57, 3],
-        [57, 90],
-        [1, 90],
-      ], // Bottom Left
+      name: "Tri-Valley 1", // new
+      coords: [[246, 209],
+        [312, 209],
+        [312, 279],
+        [246, 279]
+      ],
       color: "#10B981",
     },
     {
-      name: "W1AW/6",
-      coords: [
-        [1, 3],
-        [57, 3],
-        [57, 90],
-        [1, 90],
-      ], // Bottom Left
+      name: "Tri-Valley 2", // new
+      coords: [[246, 131],
+        [311, 131],
+        [311, 192],
+        [246, 192]
+      ],
       color: "#10B981",
     },
     {
-      name: "Expo",
-      coords: [
-        [1, 3],
-        [57, 3],
-        [57, 90],
-        [1, 90],
-      ], // Bottom Left
+      name: "Prize Booth", // new
+      coords: [[593, 293],
+        [612, 293],
+        [612, 320],
+        [593, 320]
+      ],
       color: "#10B981",
     },
     {
-      name: "Salon 2 (75)",
+      name: "W1AW/6", // new
       coords: [
-        [1, 3],
-        [57, 3],
-        [57, 90],
-        [1, 90],
-      ], // Bottom Left
+        [835, 705],
+        [910, 705],
+        [910, 776],
+        [835, 776]
+      ],
       color: "#10B981",
     },
     {
-      name: "Salon E",
+      name: "Salon A-D", // new
       coords: [
-        [55, 310],
-        [215, 310],
-        [215, 413],
-        [55, 413],
-      ], // Center-Right Large
+        [969, 425],
+        [1124, 425],
+        [1124, 604],
+        [969, 604]
+      ],
+      color: "#10B981",
+    },
+    {
+      name: "Salon 1",
+      coords: [[712, 244],
+        [898, 244],
+        [898, 386],
+        [712, 386]
+      ],
+      color: "#10B981",
+    },
+    {
+      name: "Salon 2 (75)", // new
+      coords: [[913, 302],
+        [966, 302],
+        [966, 385],
+        [913, 385]
+      ],
+      color: "#10B981",
+    },
+    {
+      name: "Salon E", // new
+      coords: [[913, 302],
+        [966, 302],
+        [966, 385],
+        [913, 385]
+      ],
       color: "#3B82F6",
     },
     {
-      name: "Salon H",
-      coords: [
-        [169, 414],
-        [215, 414],
-        [215, 487],
-        [169, 487],
-      ], // Top Right Stack
-      color: "#F59E0B",
-    },
-    {
-      name: "Salon G",
-      coords: [
-        [109, 414],
-        [168, 414],
-        [168, 487],
-        [109, 487],
-      ], // Mid Right Stack
-      color: "#F59E0B",
-    },
-    {
-      name: "Grand Ballroom, E, F, G & H Combined",
-      coords: [
-        [57, 311],
-        [214, 311],
-        [214, 488],
-        [57, 488],
+      name: "Salon F", // new
+      coords: [[968, 712],
+        [1017, 712],
+        [1017, 785],
+        [968, 785]
       ],
       color: "#F59E0B",
     },
     {
-      name: "Salon F",
-      coords: [
-        [55, 414],
-        [108, 414],
-        [108, 485],
-        [55, 485],
-      ], // Bottom Right Stack
+      name: "Salon G", // new
+      coords: [[1018, 712],
+          [1080, 712],
+          [1080, 784],
+          [1018, 784]
+      ],
       color: "#F59E0B",
     },
     {
-      name: "Pleasanton",
-      coords: [
-        [193, 518],
-        [255, 518],
-        [255, 581],
-        [193, 581],
-      ], // Far Right Top
+      name: "Salon H", // new
+      coords: [[1081, 711],
+        [1125, 711],
+        [1125, 786],
+        [1081, 786]
+      ],
+      color: "#F59E0B",
+    },
+    {
+      name: "Salons E,F,G,H (300)", // new
+      coords: [[969, 608],
+        [1123, 608],
+        [1123, 784],
+        [969, 784]
+      ],
+      color: "#F59E0B",
+    },
+    {
+      name: "Pleasanton,Danville (100)", // new
+      coords: [[1046, 816],
+        [1165, 816],
+        [1165, 875],
+        [1046, 875]
+      ],
       color: "#8B5CF6",
     },
     {
-      name: "Danville",
-      coords: [
-        [135, 518],
-        [192, 518],
-        [192, 581],
-        [135, 581],
-      ], // Far Right Mid
+      name: "San Ramon Boardroom", // new
+      coords: [[971, 815],
+        [1045, 815],
+        [1045, 875],
+        [971, 875]
+      ],
       color: "#8B5CF6",
     },
     {
-      name: "San Ramon Boardroom",
-      coords: [
-        [58, 518],
-        [134, 518],
-        [134, 581],
-        [58, 581],
-      ], // Far Right Bottom
-      color: "#8B5CF6",
-    },
-    {
-      name: "Restrooms-Bishop-Hallway",
-      coords: [
-        [8, 488],
-        [34, 488],
-        [34, 531],
-        [8, 531],
+      name: "Restrooms, Bishop Hallway", // new
+      coords: [[833, 787],
+        [914, 787],
+        [914, 878],
+        [833, 878]
       ],
       color: "#005CF6",
     },
     {
-      name: "Restrooms-Bar",
-      coords: [
-        [8, 488],
-        [34, 488],
-        [34, 531],
-        [8, 531],
+      name: "Restrooms, Bar", // new
+      coords: [[749, 182],
+        [823, 182],
+        [823, 241],
+        [749, 241]
+      ],
+      color: "#005CF6",
+    },
+    {
+      name: "Elevators", // new
+      coords: [[554, 220],
+        [596, 220],
+        [596, 287],
+        [554, 287]
+      ],
+      color: "#005CF6",
+    },
+    {
+      name: "Contra Costa Patio",
+      coords: [[657, 423],
+        [861, 423],
+        [861, 482],
+        [657, 482]
+      ],
+      color: "#005CF6",
+    },
+    {
+      name: "Bishop Ranch Patio",
+      coords: [[834, 500],
+        [913, 500],
+        [913, 704],
+        [834, 704]
+      ],
+      color: "#005CF6",
+    },
+    {
+      name: "Bishop Ranch Hallway Foyer",
+      coords: [[864, 457],
+        [917, 457],
+        [917, 499],
+        [864, 499]
       ],
       color: "#005CF6",
     },
